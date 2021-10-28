84d597ad7e7824233debd64c2283a987bdc3afe5
Omkar081 <715striker@gmail.com>
2021-10-27 18:02:55 -0700

    removed REVRobotics Stuff

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -8,13 +8,13 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.CANEncoder;
+/*import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Spark;*/
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.ClimbConstants;
@@ -23,14 +23,15 @@ public class ClimbSubsystem extends SubsystemBase {
   /**
    * Creates a new ClimbSubsygtem.
    */
-  //WPI_TalonSRX rightLifter = new WPI_TalonSRX(ClimbConstants.rightClimberID);
-  //WPI_TalonSRX leftLifter = new WPI_TalonSRX(ClimbConstants.leftClimberID);
-  CANSparkMax rightLifter = new CANSparkMax(ClimbConstants.rightClimberID, MotorType.kBrushless);
-  CANSparkMax leftLifter = new CANSparkMax(ClimbConstants.leftClimberID, MotorType.kBrushless);
+  WPI_TalonSRX rightLifter = new WPI_TalonSRX(ClimbConstants.rightClimberID);
+  WPI_TalonSRX leftLifter = new WPI_TalonSRX(ClimbConstants.leftClimberID);
+  //CANSparkMax rightLifter = new CANSparkMax(ClimbConstants.rightClimberID, MotorType.kBrushless);
+  //CANSparkMax leftLifter = new CANSparkMax(ClimbConstants.leftClimberID, MotorType.kBrushless);
   
   SpeedControllerGroup lifters = new SpeedControllerGroup(leftLifter, rightLifter);
 
-  CANEncoder rightEncoder = rightLifter.getEncoder();
+  //CANEncoder rightEncoder = rightLifter.getEncoder();
+  
   
 
   public void setSpeed(double speed) {
@@ -47,7 +48,7 @@ public class ClimbSubsystem extends SubsystemBase {
   }
 
   public double getClimbHeight() {
-  return rightEncoder.getPosition();
+  return rightLifter.getSelectedSensorPosition();
   }
 
   public void stopClimb(){
